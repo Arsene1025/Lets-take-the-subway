4b9ddf8351938d778c3d740d998bac72e6518038
blumous <50953717+blumouse@users.noreply.github.com>
2026-09-16 16:11:03 +0900

    feat: 메인 ui 작동 테스트 등
    - pathUI 동작 적용
    - 미반영/변경된 ui 에셋 반영

--- Source/LetsTakeTheSubway/UI/Guide/GuideType.h
+++ Source/LetsTakeTheSubway/UI/Guide/GuideType.h
@@ -8,11 +8,11 @@ UENUM(BlueprintType)
 enum class EGuideType : uint8
 {
     None        UMETA(DisplayName = "None"),
-    Tutorial    UMETA(DisplayName = "Tutorial"),
+    Tutorial1   UMETA(DisplayName = "Tutorial1"),
+    Tutorial2   UMETA(DisplayName = "Tutorial2"),
     Stage1_1    UMETA(DisplayName = "Stage1_1"),
     Stage1_2    UMETA(DisplayName = "Stage1_2"),
     Stage1_3    UMETA(DisplayName = "Stage1_3"),
-    Stage1_4    UMETA(DisplayName = "Stage1_4"),
     Stage2_1    UMETA(DisplayName = "Stage2_1"),
 
     MAXVALUE    UMETA(DisplayName = "MAXVALUE"),
